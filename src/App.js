0d7884c55545ff8f80770a00689ef52bae966287
Aleksander Kruk <kruk.aleksander@hotmail.com>
2021-10-24 08:41:50 +0200

    create file for other components, finish offer component for desktop

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import "normalize.css";
 import "./css/main.min.css";
 import Header from "./components/Header";
 import { Start } from "./components/Start";
+import Offer from "./components/Offer";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
     <div className="wrapper">
       <Header />
       <Start />
+      <Offer />
+      <About />
+      <Contact />
     </div>
   );
 };
---
+++ src/components/Offer.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+const Offer = () => {
+  return (
+    <div className="offer">
+      <section className="offer__section offer__section--market">
+        <h2 className="offer__heading">Co tak naprawdę mam na fakturze?</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione in et
+          commodi nostrum voluptates, est quisquam tempora ducimus repellat
+          eligendi adipisci pariatur eius itaque aperiam dolorem rem sequi
+          placeat. Nam!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione in et
+          commodi nostrum voluptates, est quisquam tempora ducimus repellat
+          eligendi adipisci pariatur eius itaque aperiam dolorem rem sequi
+          placeat. Nam!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione in et
+          commodi nostrum voluptates, est quisquam tempora ducimus repellat
+          eligendi adipisci pariatur eius itaque aperiam dolorem rem sequi
+          placeat. Nam!
+        </p>
+      </section>
+      <section className="offer__section offer__section--solution">
+        <h2 className="offer__heading">Co mogę z tym zrobić?</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione in et
+          commodi nostrum voluptates, est quisquam tempora ducimus repellat
+          eligendi adipisci pariatur eius itaque aperiam dolorem rem sequi
+          placeat. Nam!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione in et
+          commodi nostrum voluptates, est quisquam tempora ducimus repellat
+          eligendi adipisci pariatur eius itaque aperiam dolorem rem sequi
+          placeat. Nam!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione in et
+          commodi nostrum voluptates, est quisquam tempora ducimus repellat
+          eligendi adipisci pariatur eius itaque aperiam dolorem rem sequi
+          placeat. Nam!
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default Offer;
